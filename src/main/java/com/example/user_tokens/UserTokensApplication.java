d019f55f668907013042447e6aabfd900bd8e637
artoy <artoym17mel@tut.by>
2021-09-27 21:46:09 +0300

    init commit

---
+++ src/main/java/com/example/user_tokens/UserTokensApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_tokens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTokensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTokensApplication.class, args);
+    }
+
+}
